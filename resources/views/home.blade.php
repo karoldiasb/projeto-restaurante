75a0bc843edd2578ea6e474055f578f779795f48
Karoline Dias Barreto <karolinedbarreto@gmail.com>
2021-08-29 12:54:15 -0300

    build: add bootstrap

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurante</title>
+
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+</head>
+<body>
+    <script src="{{ asset('js/jquery.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.js') }}"></script>
+</body>
+</html>
